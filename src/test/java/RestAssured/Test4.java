d76433e30a29167a0813b24743c31730283c389c
ADMIN <ADMIN@DESKTOP-0M62O32>
2022-10-08 12:33:21 +0530

    Updated Test 3 class and Added new Test4 class

--- src/test/java/RestAssured/Test4.java
+++ src/test/java/RestAssured/Test4.java
@@ -1,11 +1,10 @@
 package RestAssured;
 
-public class Test3 {
+public class Test4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("Hello Test updated");
+		System.out.println("New Class Test4 Added");
 
 	}
 
